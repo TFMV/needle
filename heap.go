18ab694fd2e5b6f3649786e86a02c105bfcaa858
mcgeehan <mcgeehan@gmail.com>
2026-03-24 01:50:52 +0000

    Further optimize

---
+++ heap.go
@@ -0,0 +1,39 @@
+package needle
+
+// minHeap is a min-heap of candidates.
+type minHeap []*candidate
+
+func (h minHeap) Len() int           { return len(h) }
+func (h minHeap) Less(i, j int) bool { return h[i].dist < h[j].dist }
+func (h minHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *minHeap) Push(x any) {
+	*h = append(*h, x.(*candidate))
+}
+
+func (h *minHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// maxHeap is a max-heap of candidates.
+type maxHeap []*candidate
+
+func (h maxHeap) Len() int           { return len(h) }
+func (h maxHeap) Less(i, j int) bool { return h[i].dist > h[j].dist }
+func (h maxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *maxHeap) Push(x any) {
+	*h = append(*h, x.(*candidate))
+}
+
+func (h *maxHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
